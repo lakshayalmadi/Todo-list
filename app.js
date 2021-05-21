af661b9db70ff65ec74e3e24b904d7c720003502
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-05-21 20:48:55 +0530

    added-todofeature

--- app.js
+++ app.js
@@ -3,8 +3,31 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items =["test1", "test2"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req,res){
-    res.send("Hello");
+    var today = new Date();
+
+    var options = {
+        weekday : "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+
+res.render('lists', {day: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
